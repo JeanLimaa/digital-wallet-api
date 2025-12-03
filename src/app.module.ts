51627334dfff77d045f982566050712319054a9d
Jean Lima <jeansantoslima17@gmail.com>
2025-12-02 21:16:55 -0300

    feat: configure application module with necessary imports and providers

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,27 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { APP_INTERCEPTOR } from '@nestjs/core';
+import { PrismaService } from './common/services/prisma.service';
+import { UserModule } from './modules/user/user.module';
+import { AuthModule } from './modules/auth/auth.module';
+import { TransactionModule } from './modules/transaction/transaction.module';
+import { LoggerModule } from './modules/logger/logger.module';
+import { LoggingInterceptor } from './common/interceptors/logging.interceptor';
+import { HealthModule } from './modules/health/health.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    LoggerModule,
+    HealthModule,
+    UserModule,
+    AuthModule,
+    TransactionModule,
+  ],
+  providers: [
+    PrismaService,
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: LoggingInterceptor,
+    },
+  ],
 })
 export class AppModule {}
